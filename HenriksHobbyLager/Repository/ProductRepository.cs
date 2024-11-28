074ce99556022d8c3984a9363a623d8c822c3ade
Yotaka k. <88yotkha@gafe.molndal.se>
2024-11-28 12:46:45 +0100

    refactor : connecting database with repository class

--- HenriksHobbyLager/Repository/ProductRepository.cs
+++ HenriksHobbyLager/Repository/ProductRepository.cs
@@ -2,74 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using HenriksHobbyLager.Interfaces;
+using HenriksHobbyLager.Database;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace HenriksHobbyLager.Repositories
 {
     public class Repository<T> : IRepository<T> where T : class
     {
-        private readonly List<T> _entities;
+        // Database context for managing database connections and operations
+        private readonly DbContext _context;
+        // DbSet for managing the entities of type T
+        private readonly DbSet<T> _dbSet;
 
-        private int _nextId;
-
-        public Repository()
+        
+        // Constructor for the Repository class
+        // Constructor accepts the DbContext (injected via dependency injection)
+        public Repository(DbContext context)
         {
-            _entities = new List<T>();
-            _nextId = 1;
+            _context = context;
+            _dbSet = context.Set<T>();
+            
         }
 
+        // Get all entities from the database
         public IEnumerable<T> GetAll()
         {
-            return _entities;
+            return _dbSet.ToList();
         }
-// get id properties from class 
+        // Get an entity by its ID
         public T GetById(int id)
         {
             var property = typeof(T).GetProperty("Id");
             if (property == null)
                 throw new InvalidOperationException("T måste ha en Id-egenskap.");
 
-            return _entities.Find(e => (int?)property.GetValue(e) == id) ?? 
+            return _dbSet.Find(id) ?? 
                    throw new InvalidOperationException($"Entity with id {id} not found.");
         }
 
+        // Add an entity to the database
         public void Add(T entity)
         {
-            var property = typeof(T).GetProperty("Id");
-            if (property == null)
-                throw new InvalidOperationException("T måste ha en Id-egenskap.");
-
-            property.SetValue(entity, _nextId++);
-            _entities.Add(entity);
+            _dbSet.Add(entity);
+            _context.SaveChanges();
         }
-
+        // Update an entity in the database
         public void Update(T entity)
         {
-            var property = typeof(T).GetProperty("Id");
-            if (property == null)
-                throw new InvalidOperationException("T måste ha en Id-egenskap.");
-
-            int id = (int)(property.GetValue(entity) ?? throw new InvalidOperationException("Id cannot be null"));
-            var existingEntity = GetById(id);
-            if (existingEntity != null)
-            {
-                var index = _entities.IndexOf(existingEntity);
-                _entities[index] = entity;
-            }
+            _dbSet.Update(entity);
+            _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
             var entity = GetById(id);
-            if (entity != null)
-            {
-                _entities.Remove(entity);
-            }
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
         }
-
+        // Search for entities based on a predicate
         public IEnumerable<T> Search(Func<T, bool> predicate)
         {
-            return _entities.Where(predicate);
+            return _dbSet.Where(predicate);
         }
 
         
