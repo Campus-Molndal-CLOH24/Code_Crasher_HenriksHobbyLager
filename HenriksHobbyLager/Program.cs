92cb889fbd94bbfcf6b3d70b21d2eceb3e591860
Yotaka k. <88yotkha@gafe.molndal.se>
2024-11-28 12:45:58 +0100

    refactor : replaced repository insted of List but it shows error maybe later?

--- HenriksHobbyLager/Program.cs
+++ HenriksHobbyLager/Program.cs
@@ -1,16 +1,26 @@
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Data.Sqlite;
 using HenriksHobbyLager.Database;
+using HenriksHobbyLager.Interfaces;
+using HenriksHobbyLager.Models;
+using HenriksHobbyLager.Repositories;
+
+
 
 namespace HenriksHobbyLager
 {
-
-
-
-
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            // Your other code here
+        }
+    }
 }
 
 
