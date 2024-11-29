374f297674c59fd028d96ec6aa02450648f451c2
Yotaka k. <88yotkha@gafe.molndal.se>
2024-11-29 22:37:06 +0100

    feat : add new class for handling with mongoDb database

---
+++ HenriksHobbyLager/Repository/MongoDbrespository.cs
@@ -0,0 +1,46 @@
+using HenriksHobbyLager.Interfaces;
+using HenriksHobbyLager.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+
+namespace HenriksHobbyLager.Repository;
+
+public class MongoDbRepository : IRepository<Product>
+{
+    private readonly IMongoCollection<Product> _productsCollections;
+    
+    public MongoDbRepository(IMongoDatabase database)
+    {
+        _productsCollections = database.GetCollection<Product>("Products");
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        return _productsCollections.Find(_ => true).ToList();
+    }
+
+    public Product GetById(int id)
+    {
+        return _productsCollections.Find(p => p.Id == id).FirstOrDefault();
+    }
+
+    public void Add(Product entity)
+    {
+        _productsCollections.InsertOne(entity);
+    }
+    public void Update(Product entity)
+    {
+        _productsCollections.ReplaceOne(p => p.Id == entity.Id, entity);
+    }
+    public void Delete(int id)
+    {
+        _productsCollections.DeleteOne(p => p.Id == id);
+    }
+    public IEnumerable<Product> Search(Func<Product, bool> predicate)
+    {
+        return _productsCollections.AsQueryable().Where(predicate).ToList(); //convert the MongoDB collection to an IQueryable
+    }
+
+}
+
