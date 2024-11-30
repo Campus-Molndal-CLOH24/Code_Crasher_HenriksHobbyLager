9dd38c25e35f608b1b4debb23412de7d620d83fc
Yotaka k. <88yotkha@gafe.molndal.se>
2024-11-30 12:25:15 +0100

    refactor: clearn main menu  and fix : json file conencting string

--- HenriksHobbyLager/Program.cs
+++ HenriksHobbyLager/Program.cs
@@ -9,6 +9,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.IO;
 using HenriksHobbyLager.UIs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using HenriksHobbyLager.Facade;
+using HenriksHobbyLager.Repository;
+using HenriksHobbyLager.Interfaces;
 
 
 namespace HenriksHobbyLager
@@ -17,8 +22,27 @@ namespace HenriksHobbyLager
     {
         static void Main(string[] args)
         {
-            var menuHandler = new ConsoleMenuHandler();
-            menuHandler.ShowMainMenu();
+            // Load configuration
+        var _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Create the DatabaseFactory using the configuration
+        var databaseFactory = new DatabaseFactory(_configuration);
+
+        // Get repository through DatabaseMenu
+        var databaseMenu = new DatabaseMenu(databaseFactory);
+        var repository = databaseMenu.GetSelectedRepository();
+
+        // Create facade with repository
+        var productFacade = new ProductFacade(repository);
+
+        // Instantiate ConsoleMenuHandler with dependencies
+        var menuHandler = new ConsoleMenuHandler(_configuration, productFacade, databaseFactory);
+
+        // Show the main menu
+        menuHandler.ShowMainMenu();
         }
     }
 }
